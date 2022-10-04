0e6c1d8661876c47f4afe851e6199431b33a9594
mpuchyr <matt.puchyr@gmail.com>
2022-10-04 16:48:44 +0000

    Change p statements to puts for aesthetics

--- qr.rb
+++ qr.rb
@@ -1,41 +1,41 @@
 require "rqrcode"
 
 while true
-  p "What would you like to generate a qr code for (choose a number)?"
-  p "1. Web address"
-  p "2. SMS Message"
-  p "3. WiFi Network"
-  p "4. Exit the program"
+  puts "What would you like to generate a qr code for (choose a number)?"
+  puts "1. Web address"
+  puts "2. SMS Message"
+  puts "3. WiFi Network"
+  puts "4. Exit the program"
 
   user_choice = gets.chomp
   choice_num = user_choice.to_i
 
   case choice_num
     when 1
-      p "Please enter the web address"
+      puts "Please enter the web address"
       web_address = gets.chomp
       qr_code = RQRCode::QRCode.new("#{web_address}")
     when 2
-      p "Enter the recipient's number:"
+      puts "Enter the recipient's number:"
       phone_num = gets.chomp
 
-      p "What message would you like to send?"
+      puts "What message would you like to send?"
       message = gets.chomp
       qr_code = RQRCode::QRCode.new("SMSTO: #{phone_num}: #{message}")
     when 3
-      p "What is the network's name?"
+      puts "What is the network's name?"
       network_name = gets.chomp
-      p "What is the network's password?"
+      puts "What is the network's password?"
       network_password = gets.chomp
       qr_code = RQRCode::QRCode.new("WIFI:T:WPA;S:#{network_name};P:#{network_password};;")
     when 4
       break
     else
-      p "You did not enter a valid choice"
+      puts "You did not enter a valid choice"
   end
 
   if qr_code
-    p "What would you like to call your qr code?"
+    puts "What would you like to call your qr code?"
     qr_code_name = gets.chomp
     png = qr_code.as_png({ :size => 500 })
     IO.binwrite("#{qr_code_name}.png", png.to_s)
